1cf89621cbc1d00a6330881b3de644223deba9da
ryu <ryuchotichan@gmail.com>
2020-06-09 11:53:37 +0700

    net ninja flutter start

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        mainAxisAlignment: ,
+        children: <Widget>[
+          Text('hello me'),
+          FlatButton(
+            onPressed: (){},
+            color: Colors.amber,
+            child:Text('click')
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('instide container')
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
+
