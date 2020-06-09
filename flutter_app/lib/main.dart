4aabe30419f234dd34d34f065cc6b7110988d592
ryu <ryuchotichan@gmail.com>
2020-06-09 12:05:39 +0700

    column

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -15,21 +15,31 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Text('hello me'),
-          FlatButton(
-            onPressed: (){},
-            color: Colors.amber,
-            child:Text('click')
+          Row(
+            children: <Widget>[
+              Text('hello'),
+              Text('world'),
+            ],
           ),
           Container(
+            padding: EdgeInsets.all(20.0),
             color: Colors.cyan,
+            child: Text('one'),
+          ),
+          Container(
             padding: EdgeInsets.all(30.0),
-            child: Text('instide container')
-          )
+            color: Colors.pinkAccent,
+            child: Text('Two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Colors.amber,
+            child: Text('three'),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
